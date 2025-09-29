c95c1ac0c58f5df0ef3a84ee6a7639395f6722b5
Segunfai <segunfai111@yandex.ru>
2025-09-29 22:24:51 +0500

    Создание нового проекта для аттестации

--- src/attestation02_dungeon/core/Command.java
+++
@@ -1,7 +0,0 @@
-package attestation02_dungeon.core;
-
-import attestation02_dungeon.model.GameState;
-import java.util.List;
-
-@FunctionalInterface
-public interface Command { void execute(GameState ctx, List<String> args); }
