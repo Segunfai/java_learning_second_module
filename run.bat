183d77e88764ee315b2202ade2a3f16b0dcfd005
Segunfai <segunfai111@yandex.ru>
2025-09-30 00:36:52 +0500

    Создание нового проекта для аттестации

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
 @echo off
 chcp 65001 > nul
-echo Запуск DungeonMini...
 
 cd /d "C:\Users\Segunfai\IdeaProjects\java_learning_second_module"
 java -Dfile.encoding=UTF-8 -cp build\classes attestation02_dungeon.core.Main
